9414e3cde077015571a5a66c31847cb08544bca7
tygos <tygo.steenbergen@student.hu.nl>
2020-12-06 12:19:22 +0100

    Initial commit

---
+++ src/main/java/AdresDAO.java
@@ -0,0 +1,14 @@
+import java.sql.SQLException;
+import java.util.List;
+
+public interface AdresDAO {
+    public boolean save(Adres adres);
+
+    public boolean update(Adres adres);
+
+    public boolean delete(Adres adres);
+
+    public Adres findByReiziger(Reiziger reiziger) throws SQLException;
+
+    public List<Adres> findAll() throws SQLException;
+}
